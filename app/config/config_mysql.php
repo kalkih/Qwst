f58489d02619bfee101be96b84370606becf6057
kalkih <kalle_kihlstrom@hotmail.com>
2014-05-27 05:47:18 +0200

    First release

--- app/config/config_mysql.php
+++ app/config/config_mysql.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'dsn'     => "mysql:host=blu-ray.student.bth.se;dbname=kaki13;",
-    'username'        => "kaki13",
-    'password'        => "o$;nJb}7",
+    'dsn'     => "mysql:host=host;dbname=dbname;",
+    'username'        => "",
+    'password'        => "",
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     'table_prefix'    => "phpmvc_",
     'verbose' => false,
